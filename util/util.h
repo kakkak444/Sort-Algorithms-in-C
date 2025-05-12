f750365f6d4e5b459e2347ec4003546827f8fa9c
kakkak444 <172775100+kakkak444@users.noreply.github.com>
2025-05-12 21:14:46 +0900

    ユーティリティ関数の追加

--- util/util.h
+++ util/util.h
@@ -1,8 +1,21 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void print_array(const size_t n, const int arr[]) {
+#ifndef UTIL_MIN_RAND
+    #define UTIL_UTIL_H_LOCALLY_DEFINED_MIN_RAND
+    #define UTIL_MIN_RAND 0
+#endif
+
+#ifndef UTIL_MAX_RAND
+    #define UTIL_UTIL_H_LOCALLY_DEFINED_MAX_RAND
+    #define UTIL_MAX_RAND 10
+#endif
+
+
+void print_arr(const size_t n, const int arr[]) {
     printf("[ ");
 
     if (n > 0) {
@@ -12,7 +25,7 @@ void print_array(const size_t n, const int arr[]) {
         }
     }
 
-    printf("]\n");
+    printf(" ]\n");
 }
 
 int is_sorted(const size_t n, const int arr[]) {
@@ -27,3 +40,34 @@ int is_sorted(const size_t n, const int arr[]) {
 
     return 1;
 }
+
+void swap(int* const x, int* const y) {
+    if (x == y) {
+        return;
+    }
+
+    int t;
+
+    t = *x;
+    *x = *y;
+    *y = t;
+}
+
+void generate_arr(int (*const gen)(void), const size_t n, int arr[]) {
+    for (size_t i = 0;i < n;++i) {
+        arr[i] = gen();
+    }
+}
+
+int get_random(void) {
+    return UTIL_MIN_RAND + (rand() % (UTIL_MAX_RAND - UTIL_MIN_RAND));
+}
+
+
+#ifdef UTIL_UTIL_H_LOCALLY_DEFINED_MIN_RAND
+    #undef UTIL_MIN_RAND
+#endif
+
+#ifdef UTIL_UTIL_H_LOCALLY_DEFINED_MAX_RAND
+    #undef UTIL_MAX_RAND
+#endif
